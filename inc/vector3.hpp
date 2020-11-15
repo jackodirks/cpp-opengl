ac9a909e609951a847211b702fbf35e75264b342
Jacko Dirks <jacko.dirks@gmail.com>
2020-11-15 16:00:32 +0100

    Vector3: crossProduct is const

--- inc/vector3.hpp
+++ inc/vector3.hpp
@@ -7,7 +7,7 @@ class Vector3 : public Vector4 {
     public:
         Vector3();
         Vector3(const float x, const float y, const float z); 
-        Vector3 crossProduct(const Vector3 &vec3);
+        Vector3 crossProduct(const Vector3 &vec3) const;
         const float x() const;
         const float y() const;
         const float z() const;
--- src/vector3.cpp
+++ src/vector3.cpp
@@ -8,6 +8,7 @@ Vector3::Vector3(const float x, const float y, const float z)
 {}
 
 Vector3 Vector3::crossProduct(const Vector3 &vec3)
+Vector3 Vector3::crossProduct(const Vector3 &vec3) const
 {
     float x = (this->vec[1] * vec3.vec[2]) - (this->vec[2] * vec3.vec[1]);
     float y = (this->vec[2] * vec3.vec[0]) - (this->vec[0] * vec3.vec[2]);
