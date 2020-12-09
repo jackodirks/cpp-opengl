e26641af95fbda4055770cffcbc68de6f192ddbb
Jacko Dirks <jacko.dirks@gmail.com>
2020-12-09 22:31:09 +0100

    main: Display an empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,57 @@
 #include <iostream>
+#include <cmath>
 
-#include "matrix4.hpp"
-#include "vector3.hpp"
+#include "viewMatrix.hpp"
+#include "perspectiveProjectionMatrix.hpp"
 #include "openglMatrix.hpp"
 #include "shaderProgram.hpp"
 #include "GLFW/glfw3.h"
+
+#define DEGREES_TO_RADIANS(degrees) ((degrees) * M_PI / 180.0)
+
+static float vertices[] = {
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+
+    -0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f, -0.5f,
+     0.5f, -0.5f,  0.5f,
+     0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f,  0.5f,
+    -0.5f, -0.5f, -0.5f,
+
+    -0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f, -0.5f,
+     0.5f,  0.5f,  0.5f,
+     0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f,  0.5f,
+    -0.5f,  0.5f, -0.5f,
+};
 
 int main() {
 
@@ -22,6 +69,7 @@ int main() {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         return EXIT_FAILURE;
     }
+    glEnable(GL_DEPTH_TEST);
     ShaderProgram shader = []() -> ShaderProgram {
         try {
             return ShaderProgram("shaders/vertex.vert", "shaders/fragment.frag");
@@ -30,5 +78,26 @@ int main() {
             std::exit(EXIT_FAILURE);
         }
     }();
+    GLuint VBO, cubeVAO;
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    ViewMatrix viewMatrix;
+    PerspectiveProjectionMatrix projectionMatrix(DEGREES_TO_RADIANS(45.0), 800, 600, 0.1, 100);
+    OpenGlMatrix modelMatrix;
+
+    while(!glfwWindowShouldClose(window)) {
+        // Set the background
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     return 0;
 }
