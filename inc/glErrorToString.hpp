91803b5b1174df2571a3cc2d5336b3bb9aa9c6ba
Jacko Dirks <jacko.dirks@gmail.com>
2020-12-09 21:42:01 +0100

    Add glErrorToString.hpp

---
+++ inc/glErrorToString.hpp
@@ -0,0 +1,24 @@
+#ifndef GL_ERROR_TO_STRING_H
+#define GL_ERROR_TO_STRING_H
+
+inline const char* glErrorToString(const GLenum err)
+{
+    switch (err) {
+    case GL_NO_ERROR:
+        return "GL_NO_ERROR";
+    case GL_INVALID_ENUM:
+        return "GL_INVALID_ENUM";
+    case GL_INVALID_VALUE:
+        return "GL_INVALID_VALUE";
+    case GL_INVALID_OPERATION:
+        return "GL_INVALID_OPERATION";
+    case GL_OUT_OF_MEMORY:
+        return "GL_OUT_OF_MEMORY";
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+        return "GL_INVALID_FRAMEBUFFER_OPERATION";
+    default:
+        return "UNKOWN ERROR";
+    }
+}
+
+#endif //GL_ERROR_TO_STRING_H
