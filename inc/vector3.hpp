5f9b93dce65c25ed7368b2381c5fb926d2b5e6b0
Jacko Dirks <jacko.dirks@gmail.com>
2020-12-31 17:49:59 +0100

    Vector3: Add documentation

--- inc/vector3.hpp
+++ inc/vector3.hpp
@@ -2,12 +2,15 @@
 #define VECTOR3_HPP
 
 #include "vector4.hpp"
-
+/**Represents a 3x1 vector with type float. Inherits almost everything from Vector4, the fourth element (w) has an undefined value and should not be used.*/
 class Vector3 : public Vector4 {
     public:
-        Vector3(const Vector4& vec);
+        /**Construct a Vector3 from individual elements. Doubles as default constructor*/
         Vector3(const float x = 0, const float y = 0, const float z = 0);
+        /**Construct a Vector3 from a Vector4*/
+        Vector3(const Vector4& vec);
 
+        /**Calculate the cross product lhs x rhs.*/
         static Vector3 crossProduct(const Vector3& lhs, const Vector3& rhs);
 };
 
