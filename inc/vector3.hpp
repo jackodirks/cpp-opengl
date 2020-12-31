ca65129b7027542cb9c2a68dcce81c00ee9bfdf8
Jacko Dirks <jacko.dirks@gmail.com>
2020-12-31 17:49:45 +0100

    Vector3::crossProduct: Parameters are const

--- inc/vector3.hpp
+++ inc/vector3.hpp
@@ -8,7 +8,7 @@ class Vector3 : public Vector4 {
         Vector3(const Vector4& vec);
         Vector3(const float x = 0, const float y = 0, const float z = 0);
 
-        static Vector3 crossProduct(Vector3& lhs, Vector3& rhs);
+        static Vector3 crossProduct(const Vector3& lhs, const Vector3& rhs);
 };
 
 #endif //VECTOR3_HPP
--- src/vector3.cpp
+++ src/vector3.cpp
@@ -7,7 +7,7 @@ Vector3::Vector3(const float x, const float y, const float z)
 Vector3::Vector3(const Vector4& vec) : Vector4(vec)
 {}
 
-Vector3 Vector3::crossProduct(Vector3& lhs, Vector3& rhs)
+Vector3 Vector3::crossProduct(const Vector3& lhs, const Vector3& rhs)
 {
     float x = (lhs.vec[1] * rhs.vec[2]) - (lhs.vec[2] * rhs.vec[1]);
     float y = (lhs.vec[2] * rhs.vec[0]) - (lhs.vec[0] * rhs.vec[2]);
